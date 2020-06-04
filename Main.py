8a86536d1008c051a471a9084cad0d19d24f0035
Noam <noams@epd-medical.com>
2020-06-04 22:55:58 +0300

    warping correctly using features. will probably work better using cross correlation (sliding)

--- Main.py
+++ Main.py
@@ -23,24 +23,34 @@ if __name__ == "__main__":
         reference = cv2.imread(config.data.reference_image_path, 0)
         diff = np.abs(inspected - reference)
 
-        # plt.figure()
-        # plt.title('inspected')
-        # plt.imshow(inspected)
-        # plt.figure()
-        # plt.title('reference')
-        # plt.imshow(reference)
-        # plt.figure()
-        # plt.title('diff')
-        # plt.imshow(diff)
-        #
-        # plt.show()
+        plt.figure()
+        plt.title('inspected')
+        plt.imshow(inspected)
+        plt.figure()
+        plt.title('reference')
+        plt.imshow(reference)
+        plt.figure()
+        plt.title('diff')
+        plt.imshow(diff)
+
+
+        r_blured = cv2.blur(reference, (config.alignment.blur_radius, config.alignment.blur_radius))
+        i_blured = cv2.blur(inspected, (config.alignment.blur_radius, config.alignment.blur_radius))
 
         from alignment.Aligner import Aligner
         aligner = Aligner()
-        matches = aligner.align(reference, inspected)
+        matches_image, warped = aligner.align_using_feature_matching(static=r_blured, moving=i_blured)
         plt.figure()
-        plt.title('matches')
-        plt.imshow(matches)
+        plt.title('matches_image')
+        plt.imshow(matches_image)
+        plt.figure()
+        plt.title('warped')
+        plt.imshow(warped)
+
+        warped_diff = np.abs(warped - reference)
+        plt.figure()
+        plt.title('warped_diff')
+        plt.imshow(warped_diff)
 
         plt.show()
     main()
--- alignment/Aligner.py
+++ alignment/Aligner.py
@@ -1,25 +1,41 @@
 import cv2
 from Utils.ConfigProvider import ConfigProvider
+import numpy as np
 
 
 class Aligner(object):
     def __init__(self):
         self._config = ConfigProvider.config()
         self._blur_radius = self._config.alignment.blur_radius
+        self._min_match_distance = self._config.alignment.min_match_distance
 
         self._detector = cv2.ORB_create()
         self._matcher = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
 
-    def align(self, static, moving):
-        s_blured = cv2.blur(static, (self._blur_radius, self._blur_radius))
-        m_blured = cv2.blur(moving, (self._blur_radius, self._blur_radius))
-
-        kp1, des1 = self._detector.detectAndCompute(s_blured, None)
-        kp2, des2 = self._detector.detectAndCompute(m_blured, None)
+    def align_using_feature_matching(self, static, moving):
+        kp1, des1 = self._detector.detectAndCompute(static, None)
+        kp2, des2 = self._detector.detectAndCompute(moving, None)
         matches = self._matcher.match(des1, des2)
-        matches = sorted(matches, key=lambda x: x.distance)
-        matches_image = cv2.drawMatches(static, kp1, moving, kp2, matches[:10], None,
-                              flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
 
-        return matches_image
+        # matches = sorted(filter(lambda x: x.distance < self._min_match_distance, matches), key=lambda x: x.distance)
+        # matches_image = cv2.drawMatches(static, kp1, moving, kp2, matches[:10], None,
+        #                                 flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
 
+        assert len(matches) >= 3  # for affine tform
+        moving_pts = np.float32([kp1[m.queryIdx].pt for m in matches]).reshape(-1, 1, 2)
+        static_pts = np.float32([kp2[m.trainIdx].pt for m in matches]).reshape(-1, 1, 2)
+        tform, mask = cv2.findHomography(moving_pts, static_pts, cv2.RANSAC, 5.0)
+        matches_mask = mask.ravel().tolist()
+
+        matches_image = cv2.drawMatches(static, kp1, moving, kp2, matches, None,
+                                        flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS,
+                                        matchesMask=matches_mask,
+                                        matchColor=(0, 255, 0),)
+
+        warped = cv2.warpPerspective(moving, np.linalg.pinv(tform), (static.shape[1], static.shape[0]))
+
+        # return matches_image
+        return matches_image, warped
+
+    def align_using_normxcorr(self, static, moving):
+        pass
