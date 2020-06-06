f955663c4c1532582aa8593e36269cb984917d01
Noam <noams@epd-medical.com>
2020-06-06 23:42:08 +0300

    prepared false positives cleaner

--- noise_cleaning/NoiseCleaner.py
+++ noise_cleaning/NoiseCleaner.py
@@ -34,9 +34,9 @@ class NoiseCleaner(object):
         clean = cv2.morphologyEx(image, cv2.MORPH_DILATE, kernel, iterations=3)
         return clean
 
-    def close(self, image, diameter=5):
+    def close(self, image, diameter=5, iterations=3):
         kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (diameter, diameter))
-        clean = cv2.morphologyEx(image, cv2.MORPH_CLOSE, kernel, iterations=3)
+        clean = cv2.morphologyEx(image, cv2.MORPH_CLOSE, kernel, iterations=iterations)
         return clean
 
     @staticmethod
