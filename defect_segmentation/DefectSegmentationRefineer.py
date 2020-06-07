c080d9a5669c4754745ba2b03b1757b5a9602cdc
Noam <noams@epd-medical.com>
2020-06-07 20:14:18 +0300

    fixed bug with wrong constant at frame cleaning

--- defect_segmentation/DefectSegmentationRefineer.py
+++ defect_segmentation/DefectSegmentationRefineer.py
@@ -8,7 +8,7 @@ class DefectSegmentationRefiner(object):
     def __init__(self):
         self._config = ConfigProvider.config()
         self._noise_cleaner = NoiseCleaner()
-        self._blured_diff_thres = self._config.detection.blured_diff_thres
+        self._frame_radius = self._config.detection.frame_radius
 
         self._min_diff_threshold = self._config.detection.min_diff_threshold
 
@@ -28,17 +28,17 @@ class DefectSegmentationRefiner(object):
 
         diff = np.zeros(inspected.shape, dtype=np.float32)
         diff[warp_mask] = (np.abs((np.float32(warped) - np.float32(inspected))))[warp_mask]
-        diff[self._noise_cleaner.dilate((~warp_mask).astype('uint8'), self._blured_diff_thres) > 0] = 0
+
+        # get rid of registration inaccuracy on the frame
+        diff[self._noise_cleaner.dilate((~warp_mask).astype('uint8'), self._frame_radius) > 0] = 0
 
         # enlarge detection area in case of close proximity misses
         dirty_defect_mask_dilated = \
             self._noise_cleaner.dilate(dirty_defect_mask.astype('uint8'), diameter=5).astype(np.bool)
 
         diff_above_thres_mask = self._min_diff_threshold < diff
-        clean_defect_mask = np.zeros_like(warp_mask)
-        clean_defect_mask[dirty_defect_mask_dilated > 0] = True
 
-        clean_defect_mask = np.logical_and(diff_above_thres_mask, clean_defect_mask)
+        clean_defect_mask = np.logical_and(diff_above_thres_mask, dirty_defect_mask_dilated)
 
         plot_image(inspected, "inspected")
         plot_image(dirty_defect_mask, "dirty_defect_mask")
